38b39cfc32fc17cb4c5e45f47757edde5676dcdc
Jingshi Liu <jingshiliu07@gmail.com>
2024-04-10 16:31:16 -0400

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+    return (
+        <>
+            <main className={' flex h-0 flex-col justify-center'}></main>
+        </>
+    )
+}
+
+export default App
