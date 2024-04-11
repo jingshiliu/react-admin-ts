73aa4cf2bfe6193d79d56cc80cd722050d98f145
Jingshi Liu <jingshiliu07@gmail.com>
2024-04-11 15:12:28 -0400

    code setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+
 import { useState } from 'react'
 
 function App() {
     return (
         <>
-            <main className={' flex h-0 flex-col justify-center'}></main>
+            <main className={''}>Hello</main>
         </>
     )
 }
