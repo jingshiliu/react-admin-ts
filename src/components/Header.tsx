707d91ba8221d2bb514ab072367af29f497373bf
Jingshi Liu <jingshiliu07@gmail.com>
2024-04-12 17:30:11 -0400

    chore: remove default mb=30px

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import {Typography, Box} from '@mui/material'
-import {type ColorMode, ColorModeContext, tokens} from '../theme'
+import { Typography, Box } from '@mui/material'
+import { type ColorMode, ColorModeContext, tokens } from '../theme'
 import { useContext } from 'react'
 
 interface HeaderProps {
@@ -7,16 +7,29 @@ interface HeaderProps {
     description: string
 }
 
-function Header({title, description}: HeaderProps) {
-    const {themeMode} = useContext<ColorMode>(ColorModeContext)
+function Header({ title, description }: HeaderProps) {
+    const { themeMode } = useContext<ColorMode>(ColorModeContext)
     const colors = tokens(themeMode)
 
-  return (
-    <Box mb="30px" >
-        <Typography variant="h2" sx={{color: colors.grey[100]}} fontWeight="bold" marginBottom="5px">{title}</Typography>
-        <Typography variant="body1" sx={{color: colors.greenAccent[400]}}>{description}</Typography>
-    </Box>
-  )
+    return (
+        <Box fontSize="larger">
+            <Typography
+                variant="h2"
+                sx={{ color: colors.grey[100] }}
+                fontWeight="bold"
+                marginBottom="5px"
+            >
+                {title}
+            </Typography>
+            <Typography
+                variant="body1"
+                sx={{ color: colors.greenAccent[400] }}
+                fontSize={'large'}
+            >
+                {description}
+            </Typography>
+        </Box>
+    )
 }
 
 export default Header
