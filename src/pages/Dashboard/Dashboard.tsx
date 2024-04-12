41c8b3a2900d4cbf0c9b06584a087e09c89d0db0
Jingshi Liu <jingshiliu07@gmail.com>
2024-04-12 17:30:24 -0400

    feat: implement top section

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,8 +1,35 @@
+import Header from '@/components/Header'
+import { Box, Button } from '@mui/material'
+import FileDownloadOutlinedIcon from '@mui/icons-material/FileDownloadOutlined'
+import { type ColorMode, ColorModeContext, tokens } from '@/theme'
+import { useContext } from 'react'
 
 function Dashboard() {
-  return (
-    <div>Dashboard</div>
-  )
+    const { themeMode } = useContext<ColorMode>(ColorModeContext)
+    const colors = tokens(themeMode)
+    return (
+        <Box className="p-4" component={'section'}>
+            <Box className="flex items-end justify-between" component={'section'}>
+                <Header
+                    title="Dashboard"
+                    description="Welcome to your dashboard!"
+                />
+                <Button
+                    startIcon={<FileDownloadOutlinedIcon />}
+                    sx={{
+                        color: colors.primary[100],
+                        backgroundColor: colors.blueAccent[700],
+                        paddingX: '2rem',
+                        paddingY: '0.8rem',
+                        borderRadius: '0.4rem',
+                        fontWeight: 'bold',
+                    }}
+                >
+                    Download Report
+                </Button>
+            </Box>
+        </Box>
+    )
 }
 
 export default Dashboard
