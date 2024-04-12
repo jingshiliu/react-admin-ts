1fd2ac2a74127f8a7d5ef6c078a92f3ee520ccb3
Jingshi Liu <jingshiliu07@gmail.com>
2024-04-12 11:02:03 -0400

    chore: setup routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,65 @@
 import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { ColorModeContext, useMode } from './theme'
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import Topbar from './pages/global/Topbar'
+import Sidebar from './pages/global/Sidebar'
+import Dashboard from './pages/dashboard'
+// import Team from './pages/team'
+// import Invoices from './pages/invoices'
+// import Contacts from './pages/contacts'
+// import Bar from './pages/bar'
+// import Form from './pages/form'
+// import Line from './pages/line'
+// import Pie from './pages/pie'
+// import FAQ from './pages/faq'
+// import Geography from './pages/geography'
+// import Calendar from './pages/calendar'
 
 function App() {
     const [theme, colorMode] = useMode()
 
     return (
-        <ColorModeContext.Provider value={colorMode}>
-            <ThemeProvider theme={theme}>
-                <CssBaseline />
-                <div className='app'>
-                    <main className='content'>
-                        <Topbar />
-                    </main>
-                </div>
-            </ThemeProvider>
-        </ColorModeContext.Provider>
+        <BrowserRouter>
+            <ColorModeContext.Provider value={colorMode}>
+                <ThemeProvider theme={theme}>
+                    {/* cssbaseline automatically applies theme.palette.background.default
+                 to the background of the page */}
+                    <CssBaseline />
+                    <div className="app">
+                        <Sidebar />
+                        <main className="content">
+                            <Topbar />
+                            <Routes>
+                                {/* <Route path="/" element={<Dashboard />} /> */}
+                                {/* <Route path="/team" element={<Team />} />
+                                <Route
+                                    path="/invoices"
+                                    element={<Invoices />}
+                                />
+                                <Route
+                                    path="/contacts"
+                                    element={<Contacts />}
+                                />
+                                <Route path="/bar" element={<Bar />} />
+                                <Route path="/form" element={<Form />} />
+                                <Route path="/line" element={<Line />} />
+                                <Route path="/pie" element={<Pie />} />
+                                <Route path="/faq" element={<FAQ />} />
+                                <Route
+                                    path="/geography"
+                                    element={<Geography />}
+                                />
+                                <Route
+                                    path="/calendar"
+                                    element={<Calendar />} 
+                                />*/}
+                            </Routes>
+                        </main>
+                    </div>
+                </ThemeProvider>
+            </ColorModeContext.Provider>
+        </BrowserRouter>
     )
 }
 
