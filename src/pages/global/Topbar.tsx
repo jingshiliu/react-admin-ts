97c19229d1a1bb7e612c2aecfc80ab0bd6e45c9d
Jingshi Liu <jingshiliu07@gmail.com>
2024-04-12 10:28:58 -0400

    chore: remove unused import

--- src/pages/global/Topbar.tsx
+++ src/pages/global/Topbar.tsx
@@ -9,7 +9,7 @@ import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined
 import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined'
 import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined'
 import SearchIcon from '@mui/icons-material/Search'
-import App from '@/App'
+
 
 function Topbar(props) {
     const theme: Theme = useTheme()
